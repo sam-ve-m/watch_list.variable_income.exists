f47b9a2e1a3449e1c3fcefddaa155bf25970abae
Samuel Veloso Marçal <samuel.marcal@ligainvest.com.br>
2023-01-13 10:06:49 -0300

    feat: route path with variable_income

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 fission spec init
 fission env create --spec --name wtc-list-exists-env --image nexus.sigame.com.br/fission-wacth-list-exists:0.1.0-0 --poolsize 0 --version 3 --imagepullsecret "nexus-v3" --spec
 fission fn create --spec --name wtc-list-exists-fn --env wtc-list-exists-env --code fission.py --targetcpu 80 --executortype newdeploy --maxscale 3 --requestsperpod 10000 --spec
-fission route create --spec --name wtc-list-exists-rt --method GET --url /watch_list/exists --function wtc-list-exists-fn
+fission route create --spec --name wtc-list-exists-rt --method GET --url /watch_list/variable_income/exists --function wtc-list-exists-fn
